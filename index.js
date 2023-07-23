c3f9e3dd8afafc6af862899578a75223422dc5c9
Jake Waldron <jakewaldron@gmail.com>
2023-07-23 15:56:05 -0400

    clean up

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import runAutomation from './automation.js';
 import playTodaysMeditation from './playTodays.js';
 
-// const system = process.argv[2] || 'macOS';
-
 const system = process.platform === 'darwin' ? 'macOS' : 'raspPi';
 console.log(system);
 
