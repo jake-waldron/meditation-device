05eda1a999d0ba33e4dc6229042fbf9f728b3c4e
Jake Waldron <jakewaldron@gmail.com>
2023-07-28 12:39:11 -0400

    gpio test

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 import playTodaysMeditation from './playTodays.js';
+import { Gpio } from 'pigpio';
+
+// const Gpio = require('pigpio').Gpio;
+
+const button = new Gpio(23, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_UP,
+	alert: true,
+});
+
+let count = 0;
+
+// Level must be stable for 10 ms before an alert event is emitted.
+button.glitchFilter(10000);
+
+button.on('alert', (level, tick) => {
+	if (level === 0) {
+		console.log(++count);
+	}
+});
 
 const system = process.platform === 'darwin' ? 'macOS' : 'raspPi';
 console.log(system);
 
-try {
-	playTodaysMeditation(system);
-} catch (error) {
-	console.log(error);
-}
+// try {
+// 	playTodaysMeditation(system);
+// } catch (error) {
+// 	console.log(error);
+// }
