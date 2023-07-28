e5970eaad692d6d5f699f977349e252307d7902b
Jake Waldron <jakewaldron@gmail.com>
2023-07-28 13:11:00 -0400

    gpio test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import playTodaysMeditation from './playTodays.js';
 import ArrayGpio from 'array-gpio';
 
-let sw = ArrayGpio.in(26);
+let sw = ArrayGpio.setInput(26);
 
 // Pressing the switch sw button, the led will turn on
 // Releasing the switch sw button, the led will turn off
@@ -11,6 +11,12 @@ sw.watch((state) => {
 
 const system = process.platform === 'darwin' ? 'macOS' : 'raspPi';
 console.log(system);
+
+setInterval(() => {
+	sw.read((state) => {
+		console.log(state);
+	});
+}, 1000);
 
 // try {
 // 	playTodaysMeditation(system);
