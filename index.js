03d9a7f17f0b4b7c8f3d1cd5accbbb228b4bb366
Jake Waldron <jakewaldron@gmail.com>
2023-07-28 13:16:47 -0400

    gpio test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import playTodaysMeditation from './playTodays.js';
 import ArrayGpio from 'array-gpio';
 
-let sw = ArrayGpio.setInput(26);
+let sw = ArrayGpio.setInput(37);
 sw.setR('pu');
 
 // Pressing the switch sw button, the led will turn on
