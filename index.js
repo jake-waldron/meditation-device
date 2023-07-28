22cf0d78b63a1e5a092e4c735ac3c1e517e9bada
Jake Waldron <jakewaldron@gmail.com>
2023-07-28 16:18:54 -0400

    update

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (system === 'raspPi') {
 			if (state === false && audioPlaying === false) {
 				audioPlaying = true;
 				console.log('button pressed');
-				playMp3RaspPi('./audio/5min.mp3')
+				playMp3RaspPi('./audio/10min.mp3')
 					.then(() => {
 						audioPlaying = false;
 					})
