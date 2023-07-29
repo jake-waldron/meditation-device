0026b56f66bae7d695545242d6de502bf497f2fe
Jake Waldron <jakewaldron@gmail.com>
2023-07-29 09:28:54 -0400

    cleanup

--- utils.js
+++ utils.js
@@ -2,22 +2,3 @@ import utils from 'util';
 import { exec } from 'child_process';
 
 export const promisifyExec = utils.promisify(exec);
-
-// async function promisifyExec(command) {
-// 	return new Promise((resolve, reject) => {
-// 		const child = exec(command, (error, stdout, stderr) => {
-// 			// if (error) {
-// 			// 	reject(error);
-// 			// } else {
-// 			// 	resolve(stdout);
-// 			// }
-// 		});
-
-// 		child.on('close', (code) => {
-// 			if (code !== 0) {
-// 				reject(new Error(`Command ${command} failed with exit code ${code}`));
-// 			}
-// 			resolve(code);
-// 		});
-// 	});
-// }
