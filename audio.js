5761a3018037698d04888c86395fdf3ac278f323
Jake Waldron <jakewaldron@gmail.com>
2023-07-29 09:39:42 -0400

    fix mac exec

--- audio.js
+++ audio.js
@@ -2,7 +2,7 @@ import { promisifyExec } from './utils.js';
 
 export default async function playMP3(mp3FileName) {
 	try {
-		await promisifyExec(`mpg321 -g 50 ${mp3FileName}`);
+		await promisifyExec(`afplay ${mp3FileName}`);
 		console.log('Audio streamed successfully!');
 	} catch (err) {
 		console.error('Failed to stream audio:', err);
