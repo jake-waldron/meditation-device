5fa2d215f738e17a8169640bb8a6b303679f10a5
Jake Waldron <jakewaldron@gmail.com>
2023-08-13 12:18:00 -0400

    refactor

---
+++ RaspPi/gpio.js
@@ -0,0 +1,111 @@
+import gpio from "array-gpio";
+
+let audioPlaying = false;
+
+const button = gpio.setInput(3);
+button.setR("pu");
+let currentButtonState;
+let lastButtonState = true;
+let longPressState = false;
+let releasingLongPress = false;
+let pushedTime;
+let timer;
+const longPressTime = 1000;
+
+const led = gpio.setOutput(8);
+const longPressLED = gpio.setOutput(40);
+
+const lengthDisplayPins = { pin : [ 38, 37, 36, 35, 33 ] };
+const lengthDisplay = gpio.setOutput(lengthDisplayPins);
+
+let lengthPosition = 0;
+const lengths = [ "3min", "5min", "10min", "15min", "20min" ];
+
+
+async function buttonHandler(state) {
+    currentButtonState = state;
+    pushedTime = Date.now();
+
+    if ( currentButtonState !== lastButtonState ) {
+
+        // button pushed
+        if ( currentButtonState === false ) {
+            handleButtonPushed();
+
+            // button released
+        } else if ( currentButtonState === true ) {
+            handleButtonReleased();
+        }
+    }
+
+    lastButtonState = currentButtonState;
+}
+
+function turnOnCurrentLength(lengthPosition) {
+    lengthDisplay.forEach((pin, index) => {
+        if ( index === lengthPosition ) {
+            pin.on();
+        } else {
+            pin.off();
+        }
+    });
+}
+
+function turnOffLengthDisplay() {
+    lengthDisplay.forEach((pin) => {
+        pin.off();
+    });
+}
+
+function handleButtonPushed() {
+    if ( longPressState === true ) { // if inside long press state
+        lengthPosition < lengths.length - 1 ? lengthPosition++ : lengthPosition = 0;
+    } else { // regular press
+        led.on();
+    }
+
+    // timer gets cleared on every button release, if it runs, enter/exit long press state
+    timer = setTimeout(() => {
+        longPressState = !longPressState;
+        longPressLED.write(longPressState);
+        if ( longPressState === true ) { // when entering long press state, turn on the length display
+            console.log("Select Length");
+            turnOnCurrentLength(lengthPosition);
+        } else { // when exiting, make sure all length pins are off, and negate the lengthPosition change from that push
+            turnOffLengthDisplay();
+            lengthPosition <= 0 ? lengthPosition = lengths.length - 1 : lengthPosition--;
+            releasingLongPress = true;
+            console.log(`Current Length: ${lengths[lengthPosition]}`);
+        }
+    }, longPressTime);
+}
+
+function handleButtonReleased() {
+    if ( longPressState && !releasingLongPress ) { // button released inside long press
+        // lengthPosition < lengths.length - 1 ? lengthPosition++ : lengthPosition = 0;
+        console.log(`Set to: ${lengths[lengthPosition]}`);
+        turnOnCurrentLength(lengthPosition);
+    }
+    if ( releasingLongPress ) { // button released after exiting long press
+        releasingLongPress = false;
+    } else if ( !longPressState && !releasingLongPress ) {
+        console.log(`Start ${lengths[lengthPosition]} Meditation`);
+        // if ( audioPlaying === false ) {
+        //     audioPlaying = true;
+        //     playMp3RaspPi(`./audio/${lengths[lengthPosition]}.mp3`)
+        //         .then(() => {
+        //             audioPlaying = false;
+        //         })
+        //         .catch((error) => {
+        //             console.log(error);
+        //             audioPlaying = false;
+        //         });
+        // }
+    }
+    led.off();
+    clearTimeout(timer);
+}
+
+export default function setupPi() {
+    button.watch(buttonHandler);
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import gpio from "array-gpio";
 import dotenv from "dotenv";
 import playMP3 from "./audio.js";
+import setupPi from "./RaspPi/gpio.js";
 
 dotenv.config();
 
@@ -25,111 +25,8 @@ const system = process.platform === "darwin" ? "macOS" : "raspPi";
 
 // -------------- Raspberry Pi Stuff ----------------
 
-let audioPlaying = false;
-
 if ( system === "raspPi" ) {
-    // set up the button
-    const button = gpio.setInput(3);
-    button.setR("pu");
-    const led = gpio.setOutput(8);
-    const longPressLED = gpio.setOutput(40);
-    const lengthDisplayPins = { pin : [ 38, 37, 36, 35, 33 ] };
-    const lengthDisplay = gpio.setOutput(lengthDisplayPins);
-
-    const longPressTime = 1000;
-
-    const lengths = [ "3min", "5min", "10min", "15min", "20min" ];
-    let lengthPosition = 0;
-
-    let currentButtonState;
-    let lastButtonState = true;
-    let pushedTime;
-
-    let timer;
-
-    let longPressState = false;
-    let releasingLongPress = false;
-
-    button.watch(async (state) => {
-        currentButtonState = state;
-        pushedTime = Date.now();
-
-        if ( currentButtonState !== lastButtonState ) {
-
-            // button pushed
-            if ( currentButtonState === false ) {
-
-                if ( longPressState === true ) { // if inside long press state
-                    lengthPosition < lengths.length - 1 ? lengthPosition++ : lengthPosition = 0;
-
-                } else { // regular press
-                    led.on();
-
-
-                }
-
-                timer = setTimeout(() => {
-                    console.log("Select Length");
-                    if ( longPressState === true ) {
-                        // if exiting long press, negate the position change from that push
-                        lengthPosition <= 0 ? lengthPosition = lengths.length - 1 : lengthPosition--;
-                        releasingLongPress = true;
-                    }
-                    longPressState = !longPressState;
-                    longPressLED.write(longPressState);
-                    if ( longPressState === true ) { // when entering long press state, turn on the length display
-                        lengthDisplay.forEach((pin, index) => {
-                            if ( index === lengthPosition ) {
-                                pin.on();
-                            } else {
-                                pin.off();
-                            }
-                        });
-                    } else { // make sure all length pins are off
-                        lengthDisplay.forEach((pin) => {
-                            pin.off();
-                        });
-                    }
-
-                    console.log(`Current Length: ${lengths[lengthPosition]}`);
-                }, longPressTime);
-
-                // button released
-            } else if ( currentButtonState === true ) {
-                if ( longPressState && !releasingLongPress ) { // button released inside long press
-                    // lengthPosition < lengths.length - 1 ? lengthPosition++ : lengthPosition = 0;
-                    console.log(`Set to: ${lengths[lengthPosition]}`);
-                    lengthDisplay.forEach((pin, index) => {
-                        if ( index === lengthPosition ) {
-                            pin.on();
-                        } else {
-                            pin.off();
-                        }
-                    });
-                }
-                if ( releasingLongPress ) {
-                    releasingLongPress = false;
-                } else if ( !longPressState && !releasingLongPress ) {
-                    console.log(`Start ${lengths[lengthPosition]} Meditation`);
-                    // if ( audioPlaying === false ) {
-                    //     audioPlaying = true;
-                    //     playMp3RaspPi(`./audio/${lengths[lengthPosition]}.mp3`)
-                    //         .then(() => {
-                    //             audioPlaying = false;
-                    //         })
-                    //         .catch((error) => {
-                    //             console.log(error);
-                    //             audioPlaying = false;
-                    //         });
-                    // }
-                }
-                led.off();
-                clearTimeout(timer);
-            }
-        }
-
-        lastButtonState = currentButtonState;
-    });
+    setupPi();
 }
 
 // -------------- Mac Stuff ----------------
