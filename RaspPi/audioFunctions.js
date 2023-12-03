562f83e586bb4010a22e270e5e06d6899f0e81fb
Jake Waldron <jakewaldron@gmail.com>
2023-12-03 17:50:55 -0500

    mpg321 options - increase buffer

--- RaspPi/audioFunctions.js
+++ RaspPi/audioFunctions.js
@@ -4,6 +4,7 @@ import mpg321 from "mpg321";
 
 const player = mpg321().remote();
 
+player.options("-b 8192");
 player.gain(50);
 
 // Function to check if Raspberry Pi is connected to Bluetooth headphones
